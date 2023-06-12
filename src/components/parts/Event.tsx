5850c17edef61182e5db3fd88a8fd95bcca7c686
Quang Nguyen <110308585+vquangn@users.noreply.github.com>
2023-06-12 15:14:24 +0200

    Extended event (#16)
    Added field Time for start time, not mandatory.
     Added field Time for end time, not mandatory.
     Added field TextLine for address.
    Added field Checkbox for "Open for external users", default is not
    checked.
    Added field Checkbox for "Collect allergy information?", default is not
    checked.

--- src/components/parts/Event.tsx
+++ src/components/parts/Event.tsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import {FetchContentResult, getUrl} from '@enonic/nextjs-adapter';
+import React from "react";
+import { FetchContentResult, getUrl } from "@enonic/nextjs-adapter";
 
-const Person = (props: FetchContentResult) => {
-    const {displayName, data, parent} = props.data?.get as any;
-    const {header, startDate, endDate, description} = data;
-    const meta = props.meta;
-    const {_path} = parent;
+const Event = (props: FetchContentResult) => {
+  const { displayName, data, parent } = props.data?.get as any;
+  const { startDate, endDate, description, startTime, endTime, location } =
+    data;
+  const meta = props.meta;
+  const { _path } = parent;
 
-    return (
-        <>
-            <div>
+  return (
+    <>
+      <div>
+        <h1>{displayName}</h1>
+        <p>{description}</p>
+        <p>Sted: {location}</p>
+        {startDate && <p>Begins: {startDate}</p>}
+        {startTime && <p>Starts at: {startTime}</p>}
+        {endDate && <p>Ends: {endDate}</p>}
+        {endTime && <p>Ends at: {endTime}</p>}
+      </div>
+      <p>
+        <a href={getUrl(_path, meta)}>Back to Events ...</a>
+      </p>
+    </>
+  );
+};
 
-                <h1>{displayName}</h1>
-                <p>{description}</p>
-                { startDate && <p>Begins: {startDate}</p> }
-                { endDate && <p>Ends: {endDate}</p> }
-            </div>
-            <p><a href={getUrl(_path, meta)}>Back to Events ...</a></p>
-        </>
-    )
-}
+export default Event;
 
-export default Person;
 
-function getTitle(photo: any, displayName: string) {
-    return (photo.attachments || [])[0].name || displayName;
-}
--- src/components/queries/getEvent.ts
+++ src/components/queries/getEvent.ts
@@ -1,4 +1,4 @@
-import {APP_NAME_UNDERSCORED} from '@enonic/nextjs-adapter';
+import { APP_NAME_UNDERSCORED } from "@enonic/nextjs-adapter";
 
 const getEvent = `
 query($path:ID!){
@@ -7,10 +7,12 @@ query($path:ID!){
       displayName
       ... on ${APP_NAME_UNDERSCORED}_Event {
         data {
-          startDate
-          endDate
           description
-
+          location
+          startDate
+          startTime
+          endDate
+          endTime
         }
       }
       parent {
