e090dc8316f90646c42aca01041a688cf0cb95cc
Bobby Westberg <bellfalasch@gmail.com>
2023-06-14 09:51:30 +0200

    Cleanup (#20)
    Co-authored-by: RisPlukken <duungtr@hotmail.com>
    Co-authored-by: Quang Nguyen <110308585+vquangn@users.noreply.github.com>

--- src/components/_mappings.ts
+++ src/components/_mappings.ts
@@ -10,41 +10,42 @@ import Person from "./views/Person";
 import MainPage from "../pages/Main";
 import ChildList, {
   childListProcessor,
-  getChildList
-} from '../parts/ChildList/ChildList';
-import Heading from '../parts/Heading';
+  getChildList,
+} from './parts/ChildList/ChildList';
 import TwoColumnLayout from './layouts/TwoColumnLayout';
-import ThreeColumnLayout from "./layouts/ThreeColumnLayout";
-import MovieDetails, { getMovie } from '../parts/MovieDetails';
-import GoogleMap from '../parts/GoogleMap/GoogleMap';
-import Countdown from '../parts/CountDown/CountDown';
-import WeatherWidget from '../parts/WeatherWidget';
-import Taxi from '../parts/Taxi/Taxi';
-import Faq from '../parts/Faq/Faq';
-
+import ThreeColumnLayout from './layouts/ThreeColumnLayout';
+import MovieDetails, { getMovie } from './parts/MovieDetails/MovieDetails';
+import GoogleMap from './parts/GoogleMap/GoogleMap';
+import Countdown from './parts/CountDown/CountDown';
+import Image from './parts/Image/Image';
+import Faq from './parts/Faq/Faq';
+import WeatherWidget from './parts/WeatherWidget/WeatherWidget';
 
 // Event
-import getEvent from "./queries/getEvent";
-import Event from "./parts/Event";
-
-//Image
-import Image from "../parts/Image/Image";
+import getEvent from './queries/getEvent';
+import Event from './parts/Event/Event';
 
 // You can set common query for all views here
 ComponentRegistry.setCommonQuery([commonQuery, commonVariables]);
 
+// *********************
 // Content type mappings
+// *********************
 ComponentRegistry.addContentType(`${APP_NAME}:person`, {
   query: getPerson,
   view: Person,
 });
 
+// *********************
 // Page mappings
+// *********************
 ComponentRegistry.addPage(`${APP_NAME}:main`, {
   view: MainPage,
 });
 
+// *********************
 // Layout mappings
+// *********************
 ComponentRegistry.addLayout(`${APP_NAME}:2-column`, {
   view: TwoColumnLayout,
 });
@@ -53,15 +54,13 @@ ComponentRegistry.addLayout(`${APP_NAME}:3-column`, {
   view: ThreeColumnLayout,
 });
 
+// *********************
 // Part mappings
+// *********************
 ComponentRegistry.addPart(`${APP_NAME}:child-list`, {
   query: getChildList,
   processor: childListProcessor,
   view: ChildList,
-});
-
-ComponentRegistry.addPart(`${APP_NAME}:heading`, {
-  view: Heading,
 });
 
 ComponentRegistry.addPart(`${APP_NAME}:movie-details`, {
@@ -81,10 +80,6 @@ ComponentRegistry.addPart(`${APP_NAME}:weatherwidget`, {
   view: WeatherWidget,
 });
 
-ComponentRegistry.addPart(`${APP_NAME}:taxi`, {
-  view: Taxi,
-});
-
 ComponentRegistry.addPart(`${APP_NAME}:event`, {
   query: getEvent,
   view: Event,
@@ -99,7 +94,9 @@ ComponentRegistry.addPart(`${APP_NAME}:faq`, {
 });
 
 /*
+// *********************
 // Debug
+// *********************
 ComponentRegistry.addContentType(CATCH_ALL, {
     view: PropsView
 });
--- src/components/parts/Image/Image.tsx
+++ src/components/parts/Image/Image.tsx
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from "react";
 import { PartProps } from "@enonic/nextjs-adapter/views/BasePart";
 //import styles from './CountDown.module.css';
 
-interface HeaderProps extends PartProps {
+interface ImageProps extends PartProps {
   header: string;
 }
 
-const Header: React.FC<HeaderProps> = ({ part }) => {
+const Image: React.FC<ImageProps> = ({ part }) => {
   const header = part?.config?.header || "";
 
   return (
-    <>
-      <div>//render picture</div>
-    </>
+      <div>TODO: render picture</div>
   );
 };
 
-export default Header;
+export default Image;
