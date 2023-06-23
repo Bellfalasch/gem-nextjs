e3bde89ca305152b22506aaab24056fb4a8ded26
Quang Nguyen <110308585+vquangn@users.noreply.github.com>
2023-06-23 09:30:11 +0200

    added RSVP and Allergy part (#26)

--- src/components/_mappings.ts
+++ src/components/_mappings.ts
@@ -22,6 +22,9 @@ import WeatherWidget from './parts/WeatherWidget/WeatherWidget';
 import getEvent from './queries/getEvent';
 import Event from './parts/Event/Event';
 
+//RSVP
+import Rsvp from "../parts/Rsvp/Rsvp";
+
 // You can set common query for all views here
 ComponentRegistry.setCommonQuery([commonQuery, commonVariables]);
 
@@ -83,6 +86,10 @@ ComponentRegistry.addPart(`${APP_NAME}:image`, {
 
 ComponentRegistry.addPart(`${APP_NAME}:faq`, {
   view: Faq,
+});
+
+ComponentRegistry.addPart(`${APP_NAME}:rsvp`, {
+  view: Rsvp,
 });
 
 /*
