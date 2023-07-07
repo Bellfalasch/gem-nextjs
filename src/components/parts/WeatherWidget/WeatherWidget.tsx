58f5c0437a7ce9de8eec1e71dfb7c8855bc142ae
Quang Nguyen <110308585+vquangn@users.noreply.github.com>
2023-07-07 14:14:40 +0200

    fix anchor and added title to the widget (#69)
    wrong text in the id anhortag.
    closes #68

--- src/components/parts/WeatherWidget/WeatherWidget.tsx
+++ src/components/parts/WeatherWidget/WeatherWidget.tsx
@@ -1,10 +1,14 @@
+import { Title } from "@gjensidige/nci-core-typography/lib/title";
 import React from "react";
 
 import style from "./Weather.module.css";
 
 const WeatherWidget: React.FC = () => {
   return (
-    <div id="partAnchor_weather" className={style.weatherPart}>
+    <div id="partAnchor_weatherwidget" className={style.weatherPart}>
+      <Title tag="h2" size="2">
+        Været
+      </Title>
       <iframe
         src="https://www.yr.no/nb/innhold/1-72837/table.html"
         title="Weather Widget"
