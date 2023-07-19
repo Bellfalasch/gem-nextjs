106ef6cf8096c770bf66d8caf8e7b9b35621f06a
Bobby Westberg <bellfalasch@gmail.com>
2023-07-19 16:16:53 +0200

    fix next/image ... ?

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-const withTM = require('next-transpile-modules')(['@enonic/nextjs-adapter']);
-
+const withTM = require("next-transpile-modules")(["@enonic/nextjs-adapter"]);
 const withEnonicCache =
-  require('@enonic/nextjs-adapter/server').withEnonicCache;
+  require("@enonic/nextjs-adapter/server").withEnonicCache;
 
 function getEnonicWebpackConfig(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
     // client-side resolution for node modules
-    fs: false
+    fs: false,
   };
   return config;
 }
@@ -16,21 +15,31 @@ async function getEnonicHeaders() {
   return [
     {
       // Apply these headers to all routes in your application.
-      source: '/:path*',
+      source: "/:path*",
       headers: [
         {
-          key: 'Content-Security-Policy',
-          value: ``
-        }
-      ]
-    }
+          key: "Content-Security-Policy",
+          value: ``,
+        },
+      ],
+    },
   ];
 }
 
 const config = {
   reactStrictMode: true,
   webpack: getEnonicWebpackConfig,
-  headers: getEnonicHeaders
+  headers: getEnonicHeaders,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8080",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = withTM(withEnonicCache(config));
