531800a9cf16cdb24d82b964a418fd70e235db4c
Bobby Westberg <bellfalasch@gmail.com>
2023-07-19 13:33:39 +0200

    chore: configure next linting for vercel

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "plugin:@next/next/recommended",
 
     // Extends two more configuration from "import" plugin
     "plugin:import/recommended",
