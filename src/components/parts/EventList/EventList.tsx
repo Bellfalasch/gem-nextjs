a7d964dac0dff595faaf71cd5df416d32b93e19c
RisPlukken <duungtr@hotmail.com>
2023-07-24 13:30:03 +0200

    Make the code shorter

--- src/components/parts/EventList/EventList.tsx
+++ src/components/parts/EventList/EventList.tsx
@@ -33,142 +33,57 @@ const EventList: React.FC = (props: FetchContentResult) => {
   const [selectedTab, setSelectedTab] = useState(0);
   const handleClick = (index: number) => setSelectedTab(index);
 
+  const eventList = (
+    <ul className={styles.ul}>
+      {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        filteredChildren.map((child: any, i: number) => (
+          <a href={getUrl(child._path, meta)}>
+            <li key={i}>
+              <div
+                className={classNames(
+                  styles["eventImage"],
+                  styles[child.data.theme]
+                )}
+              >
+                {child.data.image?.imageUrl && (
+                  <img src={getUrl(child.data.image.imageUrl, meta)} alt="" />
+                )}
+              </div>
+              <div className={styles.wrapper}>
+                <Text>{child.displayName}</Text>
+                {child.data?.location && (
+                  <p className={styles.location}>{child.data.location}</p>
+                )}
+                {child.data?.startDateTime && (
+                  <div>
+                    <Text className={styles.month}>
+                      {format(new Date(child.data.startDateTime), "MMM")}
+                    </Text>
+                    <Text className={styles.day}>
+                      {format(new Date(child.data.startDateTime), "dd")}
+                    </Text>
+                  </div>
+                )}
+              </div>
+            </li>
+          </a>
+        ))
+      }
+    </ul>
+  );
+
   return (
-    <>
-      <ToggleTabsContext selectedTab={selectedTab} setSelectedTab={handleClick}>
-        <ToggleTabs>
-          <ToggleTab label="Upcoming" index={0} />
-          <ToggleTab label="Ongoing" index={1} />
-          <ToggleTab label="Past" index={2} />
-        </ToggleTabs>
-        <ToggleTabPane index={0}>
-          <ul className={styles.ul}>
-            {
-              // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              filteredChildren.map((child: any, i: number) => (
-                <a href={getUrl(child._path, meta)}>
-                  <li key={i}>
-                    <div
-                      className={classNames(
-                        styles["eventImage"],
-                        styles[child.data.theme]
-                      )}
-                    >
-                      {child.data.image?.imageUrl && (
-                        <img
-                          src={getUrl(child.data.image.imageUrl, meta)}
-                          alt=""
-                        />
-                      )}
-                    </div>
-                    <div className={styles.wrapper}>
-                      <Text>{child.displayName}</Text>
-                      {child.data?.location && (
-                        <p className={styles.location}>{child.data.location}</p>
-                      )}
-                      {child.data?.startDateTime && (
-                        <div>
-                          <Text className={styles.month}>
-                            {format(new Date(child.data.startDateTime), "MMM")}
-                          </Text>
-                          <Text className={styles.day}>
-                            {format(new Date(child.data.startDateTime), "dd")}
-                          </Text>
-                        </div>
-                      )}
-                    </div>
-                  </li>
-                </a>
-              ))
-            }
-          </ul>
-        </ToggleTabPane>
-        <ToggleTabPane index={1}>
-          <ul className={styles.ul}>
-            {
-              // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              filteredChildren.map((child: any, i: number) => (
-                <a href={getUrl(child._path, meta)}>
-                  <li key={i}>
-                    <div
-                      className={classNames(
-                        styles["eventImage"],
-                        styles[child.data.theme]
-                      )}
-                    >
-                      {child.data.image?.imageUrl && (
-                        <img
-                          src={getUrl(child.data.image.imageUrl, meta)}
-                          alt=""
-                        />
-                      )}
-                    </div>
-                    <div className={styles.wrapper}>
-                      <Text>{child.displayName}</Text>
-                      {child.data?.location && (
-                        <p className={styles.location}>{child.data.location}</p>
-                      )}
-                      {child.data?.startDateTime && (
-                        <div>
-                          <Text className={styles.month}>
-                            {format(new Date(child.data.startDateTime), "MMM")}
-                          </Text>
-                          <Text className={styles.day}>
-                            {format(new Date(child.data.startDateTime), "dd")}
-                          </Text>
-                        </div>
-                      )}
-                    </div>
-                  </li>
-                </a>
-              ))
-            }
-          </ul>
-        </ToggleTabPane>
-        <ToggleTabPane index={2}>
-          <ul className={styles.ul}>
-            {
-              // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              filteredChildren.map((child: any, i: number) => (
-                <a href={getUrl(child._path, meta)}>
-                  <li key={i}>
-                    <div
-                      className={classNames(
-                        styles["eventImage"],
-                        styles[child.data.theme]
-                      )}
-                    >
-                      {child.data.image?.imageUrl && (
-                        <img
-                          src={getUrl(child.data.image.imageUrl, meta)}
-                          alt=""
-                        />
-                      )}
-                    </div>
-                    <div className={styles.wrapper}>
-                      <Text>{child.displayName}</Text>
-                      {child.data?.location && (
-                        <p className={styles.location}>{child.data.location}</p>
-                      )}
-                      {child.data?.startDateTime && (
-                        <div>
-                          <Text className={styles.month}>
-                            {format(new Date(child.data.startDateTime), "MMM")}
-                          </Text>
-                          <Text className={styles.day}>
-                            {format(new Date(child.data.startDateTime), "dd")}
-                          </Text>
-                        </div>
-                      )}
-                    </div>
-                  </li>
-                </a>
-              ))
-            }
-          </ul>
-        </ToggleTabPane>
-      </ToggleTabsContext>
-    </>
+    <ToggleTabsContext selectedTab={selectedTab} setSelectedTab={handleClick}>
+      <ToggleTabs>
+        <ToggleTab label="Upcoming" index={0} />
+        <ToggleTab label="Ongoing" index={1} />
+        <ToggleTab label="Past" index={2} />
+      </ToggleTabs>
+      <ToggleTabPane index={0}>{eventList}</ToggleTabPane>
+      <ToggleTabPane index={1}>{eventList}</ToggleTabPane>
+      <ToggleTabPane index={2}>{eventList}</ToggleTabPane>
+    </ToggleTabsContext>
   );
 };
 
